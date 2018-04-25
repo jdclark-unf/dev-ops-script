de1bb857b09785e49c1c60e5d66ab72ec066f64a
Blake Kruppa <blake.kruppa@wallawalla.edu>
2018-04-25 15:22:34 -0400

    Update installv2.sh

--- installv2.sh
+++ installv2.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo mkdir /usr/apache
+sudo chmod 777 /usr/apache
+
 echo "installing java 8"
 wget https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/jdk-8u171-linux-x64.rpm
 sudo yum install -y jdk-8u171-linux-x64.rpm
@@ -7,15 +10,15 @@ export PATH=/usr/java/jdk1.8.0_171-amd64/bin/
 echo "java 8 installed and configured"
 
 echo "installing maven"
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
+wget https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/apache-maven-3.5.3-bin.tar.gz
+tar xvf apache-maven-3.5.3-bin.tar.gz
+sudo mv apache-maven-3.5.3 /usr/apache
+sudo chmod 777 /usr/apache/apache-maven-3.5.3
 echo "maven installed"
 
 echo "installing jenkins"
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins -y
+https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/jenkins-2.89.4-1.1.noarch.rpm
+sudo yum install jenkins-2.89.4-1.1.noarch.rpm -y
 echo "jenkins installed now starting"
 sudo service jenkins start
 echo "jenkins started"
@@ -23,7 +26,7 @@ echo "jenkins started"
 
 echo "downloading tomcat tar.gz"
 cd /opt
-sudo wget http://mirror.cogentco.com/pub/apache/tomcat/tomcat-8/v8.5.30/bin/apache-tomcat-8.5.30.tar.gz
+sudo wget https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/apache-tomcat-8.5.30.tar.gz
 sudo tar -xzvf apache-tomcat-8.5.30.tar.gz
 echo "giving ec2-user permission for tomcat"
 sudo chmod 777 apache-tomcat-8.5.30
