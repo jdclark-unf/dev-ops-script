e0a63c46d5b75f2d3a5fc39f0439325c9af9c4b7
Blake Kruppa <blake.kruppa@wallawalla.edu>
2018-04-25 23:15:46 -0400

    Create cloud-native.sh

---
+++ cloud-native.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cd /tmp
+
+echo "installing java 8"
+wget https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/jdk-8u171-linux-x64.rpm
+sudo yum install -y jdk-8u171-linux-x64.rpm
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
+echo "java 8 installed and configured"
+
+echo "installing node"
+curl -o- https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/nvm-install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+
+echo "installing jenkins"
+wget https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/jenkins-2.89.4-1.1.noarch.rpm
+sudo yum install jenkins-2.89.4-1.1.noarch.rpm -y
+echo "jenkins installed now starting"
+sudo service jenkins start
+echo "jenkins started"
