9ca68fc0ac105e91bfb1f8f2fc1b431849d294a9
Blake Kruppa <blake.kruppa@wallawalla.edu>
2018-06-11 17:07:29 -0400

    Update cloud-native.sh

--- cloud-native.sh
+++ cloud-native.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd /tmp
 
-# echo "installing java 8"
-# wget https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/jdk-8u171-linux-x64.rpm
+echo "installing java 8"
+wget https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/jdk-8u171-linux-x64.rpm
 # sudo yum install -y jdk-8u171-linux-x64.rpm
 # sudo update-alternatives --config java
 # sudo update-alternatives --config javac
-# echo "java 8 installed and configured"
+echo "java 8 installed and configured"
 
 echo "installing node"
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
