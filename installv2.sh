8419011fe32f738d34cc1b525907f737cca470db
Blake Kruppa <btkruppa513@gmail.com>
2018-10-04 14:04:16 -0400

    Update installv2.sh
    removed java from my s3 bucket

--- installv2.sh
+++ installv2.sh
@@ -6,8 +6,7 @@ sudo chmod 777 /usr/apache
 cd /tmp
 
 echo "installing java 8"
-wget https://s3-us-west-2.amazonaws.com/blake-dev-ops-resources/jdk-8u171-linux-x64.rpm
-sudo yum install -y jdk-8u171-linux-x64.rpm
+sudo yum install -y java-1.8.0-openjdk-devel
 sudo update-alternatives --config java
 sudo update-alternatives --config javac
 echo "java 8 installed and configured"
